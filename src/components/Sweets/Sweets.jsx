8b8adb887c45dc587d8a97f8159bc3adcbca7028
Kostya2700 <kostizavali@gmail.com>
2023-04-16 08:48:11 +0300

    add sweets

--- src/components/Sweets/Sweets.jsx
+++ src/components/Sweets/Sweets.jsx
@@ -8,38 +8,16 @@ import css from "./Sweets.module.css";
 const Tweets = () => {
   const arrContacts = useSelector(getStateUser);
   const dispatch = useDispatch();
-  // const [users, setUsers] = useState([]);
   const [isFollow, setFollowings] = useState(null);
 
   useEffect(() => {
     dispatch(fetchUser());
   }, [dispatch]);
-  // const followCount = async (id) => {
-  //   setFollowings((prevFollowings) => {
-  //     const index = prevFollowings.indexOf(id);
 
-  //     setUsers((prevUsers) =>
-  //       prevUsers.map((user) => {
-  //         if (user.id === id) {
-  //           user.isFollow = !user.isFollow;
-  //           user.followers = user.isFollow
-  //             ? user.followers + 1
-  //             : user.followers - 1;
-  //         }
-  //         return user;
-  //       })
-  //     );
-
-  //     if (index === -1 || prevFollowings.lenght === 0) {
-  //       return [...prevFollowings, id];
-  //     } else {
-  //       prevFollowings.splice(index, 1);
-  //       return [...prevFollowings];
-  //     }
-  //   });
-  // };
-  const followCount = (id) => {
-    setFollowings(!true);
+  const followCount = (idUser) => {
+    const filterUser = arrContacts.filter = ({id}) => console.log(id)
+    console.log("filterUser:", filterUser)
+    setFollowings(true);
   };
 
   return (
@@ -48,15 +26,13 @@ const Tweets = () => {
       <ul className={css.list}>
         {arrContacts?.map(({ id, avatar, tweets, followers, user }) => (
           <li key={id} className={css.item}>
-            <img src={avatar} alt="<текст>" className={css.item_img} />
+            <img src={avatar} alt="user" className={css.item_img} />
             <p className={css.item_name}>{user}</p>
-            <span className={css.item_tweets}>
-              {tweets.toLocaleString()} TWEETS{" "}
-            </span>
+            <span className={css.item_tweets}>{tweets} TWEETS </span>
             <span className={css.item_followers}>
-              {followers.toLocaleString()} FOLLOWERS
+              {followers.toLocaleString("en-US")} FOLLOWERS
             </span>
-            <button className={css.btn_item} onClick={() => followCount(id)}>
+            <button className={css.btn_item} onClick={(id) => followCount(id)}>
               {isFollow ? "Following" : "Follow"}
             </button>
           </li>
